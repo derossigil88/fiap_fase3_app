40df59b4fa80d1eee6ff18793cc7a4ecce5d6077
Gil <gil.rossi.aguiar@live.com>
2022-10-15 21:46:44 -0300

    Controller tela de chamada de sucesso e erro

---
+++ lib/controller/assist_controller.dart
@@ -0,0 +1,20 @@
+import 'package:abctechapp/model/assist.dart';
+import 'package:abctechapp/service/assist_service.dart';
+import 'package:get/get.dart';
+
+class AssistController extends GetxController with StateMixin<List<Assist>> {
+  late AssistService _service;
+  @override
+  void onInit() {
+    super.onInit();
+    _service = Get.find<AssistService>();
+  }
+
+  void GetAssistList() {
+    _service
+        .getAssists()
+        .then((value) => change(value, status: RxStatus.success()))
+        .onError((error, stackTrace) =>
+            change([], status: RxStatus.error(error.toString())));
+  }
+}
